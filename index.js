8454d8256553672ea4494f9947e07f2e980d40b0
unknown <biticubianca18@stud.ase.ro>
2022-05-09 12:08:13 +0300

    First routes and a router

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const messagesRouter = require('./routes/messagesRouter');
+
 const app = express();
 app.use(cors());
 
+
+// for parsing application/json
+
+app.use(bodyParser.json());
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//define routes
+app.use("/messages", messagesRouter);
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World again!')
-  });
+// app.get('/', (req, res) => {
+//     res.send('Hello World again!')
+//   });
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
   });
